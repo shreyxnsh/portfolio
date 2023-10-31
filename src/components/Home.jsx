b79e602b3b9b1cf284e9e4e08f9e3b97975c709f
shreyxnsh <shreyanshatwork@gmail.com>
2023-11-01 01:38:36 +0530

    updated description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,19 @@ const Home = () => {
     return (
       <>
         <div className=" sm:pl-40 h-screen ">
-          <div className="pl-10  flex flex-wrap md:flex-nowrap lg:pt-40 ">      
+          <div className="pl-20  flex flex-wrap md:flex-nowrap lg:pt-20 ">      
           <div className="lg:w-1/2 mr-10">
             <motion.div initial={{ opacity: 0,x:"0%",width:"100%"}}
                animate={{x:"0%", opacity: 1,width:"100%"}}
                 transition={{delay:1,duration:0.5,ease:"easeInOut"}} className=" pt-10 fonthtml">{`<p>`}</motion.div>
-           <h1 className="text-2xl font-mono transition duration-0 cursor-pointer hover:text-cyan-400 hover:duration-700 font-semibold tracking-[0.3em] opacity-75">UDITI DAS</h1>
+           <h1 className="text-2xl font-mono transition duration-0 cursor-pointer hover:text-cyan-400 hover:duration-700 font-semibold tracking-[0.3em] opacity-75">Full Stack App Developer</h1>
            <div className="relative  flex">
            <motion.div className="text-5xl  md:text-6xl lg:text-7xl z-10 font-mono font-bold"
              initial={{x:"20%",width:"100%"}}
              animate={{x:"0%",width:"0%"}}
              transition={{duration:0.5,ease:"easeInOut"}}
            
-            >DEVELOPER</motion.div>
+            >Shreyansh Jain</motion.div>
            {/* <motion.div className='right-full z-20 bg-cyan-400'
              initial={{x:"160%",width:"100%"}}
              animate={{x:"0%",width:"0%"}}
@@ -33,9 +33,8 @@ const Home = () => {
             animate={{x:"0%", opacity: 1,width:"100%"}}
              transition={{delay:1,duration:0.5,ease:"easeInOut"}}
             >
-              I'm a web developer and I like to build really cool stuffs! 
-              I like to design in Figma, code in Visual Studio Code, and deploy to the cloud.
-              </motion.div>
+              Hey! I'm a professional full stack app developer, proficient in native and cross-platform app development.<br /> I can create apps, APIs, deploy them to cloud services, everything.<br /> <br /> I'm the guy you've been searching the internet for! <br /> 
+                            </motion.div>
               <motion.div
                initial={{ opacity: 0,x:"0%",width:"100%"}}
                animate={{x:"0%", opacity: 1,width:"100%"}}
